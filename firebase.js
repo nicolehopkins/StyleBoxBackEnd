818da23f1c2158bb46f108c72d3e925efc9fcc9a
Nicole L. Hopkins <nicolehopkins@pursuit.org>
2019-03-25 22:53:41 -0400

    uncommenting out firebase file for testing

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
-// const config = {
-//     apiKey: "AIzaSyAV9qoYQwtefIneda6gdRpmTWK7B94Z0vQ",
-//     authDomain: "style-box-2bb7a.firebaseapp.com",
-//     databaseURL: "https://style-box-2bb7a.firebaseio.com",
-//     projectId: "style-box-2bb7a",
-//     storageBucket: "style-box-2bb7a.appspot.com",
-//     messagingSenderId: "162076225904"
-//   };
-//   firebase.initializeApp(config);
+const config = {
+    apiKey: "AIzaSyAV9qoYQwtefIneda6gdRpmTWK7B94Z0vQ",
+    authDomain: "style-box-2bb7a.firebaseapp.com",
+    databaseURL: "https://style-box-2bb7a.firebaseio.com",
+    projectId: "style-box-2bb7a",
+    storageBucket: "style-box-2bb7a.appspot.com",
+    messagingSenderId: "162076225904"
+  };
+  firebase.initializeApp(config);
